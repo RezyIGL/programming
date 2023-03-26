80abb6493f94330e666097036cb7f5bdf526b183
RezyIGL <onlyrezystor@mail.ru>
2023-03-26 20:54:28 +0300

    edited names for aestethics

--- tests/test_lab_4.py
+++ tests/test_lab_4.py
@@ -2,13 +2,13 @@ import unittest
 
 test_lab_4_suite = unittest.TestSuite()
 
-from test_lab_4_1 import test_Task_1
+from test_task_4_1 import test_Task_1
 test_lab_4_suite.addTest(unittest.makeSuite(test_Task_1))
 
-from test_lab_4_2 import test_Task_2
+from test_task_4_2 import test_Task_2
 test_lab_4_suite.addTest(unittest.makeSuite(test_Task_2))
 
-from test_lab_4_3 import test_Task_3
+from test_task_4_3 import test_Task_3
 test_lab_4_suite.addTest(unittest.makeSuite(test_Task_3))
 
 
