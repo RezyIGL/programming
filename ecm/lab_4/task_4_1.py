543bb353fdf87bd274d1c62967d3ce84ae570cf5
RezyIGL <onlyrezystor@mail.ru>
2023-03-26 07:38:40 +0300

    Added firstTestFile

--- ecm/lab_4/task_4_1.py
+++
@@ -1,9 +0,0 @@
-def SelectSort(nums: list) -> list:
-    temp = nums
-    for i in range(len(temp)-1):
-        j = temp.index(min(temp[i:]))
-        temp[i], temp[j] = temp[j], temp[i]
-    return temp
-
-if __name__ == "__main__":
-    SelectSort(input)    
