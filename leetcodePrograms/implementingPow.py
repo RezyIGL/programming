7eb874fead6f6b96cb4bef655490a58821f97732
RezyIGL <onlyrezystor@mail.ru>
2023-06-06 16:43:36 +0300

    lc case

---
+++ leetcodePrograms/implementingPow.py
@@ -0,0 +1,17 @@
+def pow(x: float, n: int) -> float:
+    p = 1
+    if n<0:
+        x = 1/x
+        n = abs(n)
+
+    while n:
+        if n%2:
+            p*= x
+        x*=x
+        n//=2
+    return p
+
+if __name__ == "__main__":
+    x = 2.0
+    n = -2
+    print(pow(x, n))
