b5cd9fbd97a56ad735a8753ee5ef0aaddd89e060
RezyIGL <onlyrezystor@mail.ru>
2023-07-06 00:45:13 +0300

    done with understanding c# projects editing in vsc

---
+++ C#_projects/randInt/randomInt/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int[] a = new int[5];
+
+        Random random = new();
+
+        for (int i = 0; i < 10; i++){
+            Console.WriteLine(random.Next(a.Length));
+        }
+
+    }
+}
